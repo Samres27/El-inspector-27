f85b33e73601be9637749ed6c687e752b140d7f9
samres <yososyam4@gmail.com>
2026-05-18 14:21:18 -0400

    ui correct

--- superagente27/Script/personaje.gd
+++ superagente27/Script/personaje.gd
@@ -3,7 +3,9 @@ extends Area2D
 var is_player_close = false
 @export var npc_dialogue: DialogueResource #= preload("res://Dialogos/personaje1.dialogue")
 @export var portrait: Texture2D
-
+var destino: Vector2
+var existe_destino = false
+@export var velocidad := 20
 	
 	
 func _process(delta: float):
@@ -15,6 +17,7 @@ func _ready():
 	body_exited.connect(_on_body_exited)
 	GlobalDialogue.portrait = portrait
 	$Sprite2D.visible= false
+	generar_nuevo_destino() 
 	
 func _on_body_entered(body: Node2D):
 	
@@ -38,7 +41,41 @@ var pausa = true
 var direccion: Vector2
 var posicion_anterior: Vector2
 
-
+func generar_nuevo_destino():
+	# Genera una nueva posición aleatoria dentro de un rango
+	var rango = 50 #lo cambiamos segun el rango de movimiento deseado
+	destino = global_position + Vector2(randf_range(-rango, rango), randf_range(-rango, rango))
+	existe_destino = true
+	
+#func movimiento():
+	#if existe_destino: #verificar que se tiene un destino
+		#direccion = (destino - global_position).normalized() #Direccion a tomar entre la posicion actual y el destino
+		#velocity = direccion * velocidad
+		#move_and_slide()
+		#
+		#if get_slide_collision_count() > 0:
+			#generar_nuevo_destino()
+		#
+		## Verifica si llegó al destino
+		#if global_position.distance_to(destino) < 5:
+			#velocity = Vector2.ZERO
+			#existe_destino = false
+			#await get_tree().create_timer(randf_range(0.5, 5)).timeout  # esperamos 1.5 segundos
+			#generar_nuevo_destino()
+func movimiento():
+	if existe_destino:
+		direccion = (destino - global_position).normalized()
+		
+		# Movemos la posición manualmente usando delta (puedes pasar delta como parámetro)
+		global_position += direccion * velocidad * get_physics_process_delta_time()
+		
+		# Ya no puedes usar get_slide_collision_count() aquí
+		
+		if global_position.distance_to(destino) < 5:
+			direccion = Vector2.ZERO
+			existe_destino = false
+			await get_tree().create_timer(randf_range(0.5, 5)).timeout
+			generar_nuevo_destino()
 
 func _physics_process(delta: float) -> void:
 	direccion = Vector2.ZERO #para iniciar animacion estatica
@@ -47,21 +84,10 @@ func _physics_process(delta: float) -> void:
 		
 	if is_player_close:
 		return
-	tiempo_pausa -= delta
-	if pausa:
-		
-		if tiempo_pausa <= 0.0:
-			pausa = false
-			tiempo_pausa = randf_range(1.0, 10.0) #el rango de tiempo caminando
-	else:
-		if tiempo_pausa <= 0.0:
-			pausa = true
-			tiempo_pausa = randf_range(0.5, 1) #el rango de tiempo de la pausa
-		path_follow.progress += speed * delta
-	var pos = path_follow.global_position
-	direccion = (pos - posicion_anterior).normalized()
+	
+	movimiento() 
 	$RayCast2D.target_position = direccion * 13
-	posicion_anterior = pos
+	
 
 
 func actualizar_animacion():
